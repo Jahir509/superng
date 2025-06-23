d65c73ed8c6bf79eadc62a4997c80452e3ca6a35
jahir509 <me.jahirahmed@gmail.com>
2025-06-23 15:52:46 +0600

    Initial

---
+++ src/app/orchestrator/orchestrator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orchestrator',
+  imports: [],
+  templateUrl: './orchestrator.component.html',
+  styleUrl: './orchestrator.component.scss'
+})
+export class OrchestratorComponent {
+
+}
